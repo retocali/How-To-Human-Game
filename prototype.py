0f41ae33939c5ff89f32021333b39f09375fa490
6S08 <6S08@Xus-MacBook-Pro.local>
2017-07-11 10:36:32 -0400

    aaa

--- prototype.py
+++ prototype.py
@@ -9,38 +9,25 @@ places  = ["your home", "your workplace", "the streets", "a restaurant"]
 
 
 def gerundOfword(x):
-    # Gives the -ing for of a word
+    # Gives the -ing form of a word
     return x[:-1]+(x[-1] if x[-1] != 'e' else '') +"ing"
 
 
 def parse(words):
     personCount = 0
     placeCount = 0
-
-    time = 6
+    
     singles, combos, non_adjacent_combos = setup(personCount, placeCount);
     first_part = set()
     phrases = []
     for i in range(len(words)):
-        # Calculate Time
-        current_time = ""
-        if (time % 12 == 0):
-            current_time += "12:00"
-        else:
-            current_time += str(time % 12)+":00"
-        if (time//12):
-            current_time += "PM: "
-        else:
-            current_time += "AM: "
-        time += 2
-        
-
         # Update persons and places
-        placeCount, personCount = singles.get(words[i])[1]
+        placeCount  = (placeCount  + singles.get(words[i])[1][0]) % len(places)
+        personCount = (personCount + singles.get(words[i])[1][1]) % len(persons)
         singles, combos, non_adjacent_combos = setup(personCount, placeCount);
 
         # Single Inputs
-        phrases.append(current_time+singles.get(words[i], "")[0])
+        phrases.append(singles.get(words[i], "")[0])
 
         # Check for non-adjacent combos
         if words[i] in non_adjacent_combos:
@@ -59,41 +46,45 @@ def parse(words):
             continue # To avoid an index out of range error
         phrases.append(combos.get((words[i], words[i+1], words[i+2]), ""))
     return phrases
+        
 
+
+
+
+    
 
 
 def setup(personCount, placeCount):
     # Single Inputs
-    p = personCount
-    a = placeCount
     singles = {
     #   word    : standard output                         change in places/persons
-        "flirt" : ("You flirt with your " + persons[personCount],  ( a , p )),
-        "smile" : ("You smile with your " + persons[personCount],  ( a , p )),
-        "fight" : ("You get mad at your " + persons[personCount],  ( a ,p+1)),
-        "wake"  : ("Your eyes start to open",                      ( a , p )),
-        "work"  : ("You work with your " + persons[personCount],   ( 2 , p )),
-        "dream" : ("You dream about random things for a while",    ( a , p )),
-        "eat"   : ("You are eating at " + places[placeCount],      ( a , p )),
-        "talk"  : ("You talk with your " + persons[personCount],   ( a , p )),
-        "cry"   : ("Tears start to fall down your face",           ( a , p )),
-        "run"   : ("You run to " + places[placeCount],             (a+1,p+1)),
-        "sleep" : ("You sleep at " + places[placeCount],           ( a , p )),
-        "browse": ("You go on the internet",                       ( a , p ))
+        "flirt" : ("You flirt with your " + persons[personCount],  (0,0)),
+        "smile" : ("You smile with your " + persons[personCount],  (0,0)),
+        "fight" : ("You get mad at your " + persons[personCount],  (0,1)),
+        "wake"  : ("Your eyes start to open",                      (0,0)),
+        "work"  : ("You work with your " + persons[personCount],   (1,1)),
+        "dream" : ("You dream about random things for a while",    (0,0)),
+        "eat"   : ("You are eating at " + places[placeCount],      (0,0)),
+        "talk"  : ("You talk with your " + persons[personCount],   (0,0)),
+        "cry"   : ("Tears start to fall down your face",           (0,0)),
+        "run"   : ("You run to " + places[placeCount],             (1,1)),
+        "sleep" : ("You sleep at " + places[placeCount],           (0,0)),
+        "browse": ("You go on the internet",                       (0,0))
     }
     
     # Variable Inputs
     combos = {}
     combos.update({ (x, "cry")     : ("You %s, resulting in you crying")  % x for x in words})
     combos.update({ (x, "smile")   : ("You %s, resulting in you smiling") % x for x in words})
-    combos.update({ (x, "run")   : ("You sleepwalk") for x in sleepStuff})
 
     combos.update({ ("dream", x)   : ("You dream about " + gerundOfword(x)) for x in words})
     combos.update({ ("smile", x)   : ("You smile because you are " + gerundOfword(x)) for x in words})
 
     combos.update({("sleep" , x, "wake") : "You dream about " + gerundOfword(x)  for x in words})
 
-    combos.update({ ("flirt", "eat" , x)   : ("Your date got ruined") for x in dateStuff})
+    combos.update({ ("flirt", "eat", x)   : ("Your date got ruined") for x in dateStuff})
+    combos.update({ (x, "run")   : ("You sleepwalk") for x in sleepStuff})
+
     combos.update({ ("sleep", "talk", x)   : ("You sleeptalk about " + gerundOfword(x)) for x in sleepStuff})
     combos.update({ ("dream", "talk", x)   : ("You sleeptalk about " + gerundOfword(x)) for x in sleepStuff})
 
@@ -104,10 +95,10 @@ def setup(personCount, placeCount):
         ("eat", "fight")   : "You start a food fight",
         ("work", "browse") : "You go on reddit and pretend to do work",
         ("browse", "smile"): "You find some dank memes",
-        ("fight", "run")   : "You run away from a fight with your " + persons[personCount] + " to " + places[placeCount],
-        ("cry", "talk")    : "You cry but decide to talk it out with your " + persons[personCount],
-        ("fight", "talk")  : "You fight but decide to talk it out with your " + persons[personCount],
-        ("browse", "fights"):"You get into a heated argument through Facebook with your " + persons[personCount],
+        ("fight", "run")   : "You run away from a fight with your "+persons[personCount] + " to " + places[placeCount],
+        ("cry", "talk")    : "You cry but decide to talk it out with your "+persons[personCount],
+        ("fight", "talk")  : "You fight but decide to talk it out with your "+persons[personCount],
+        ("browse", "fights"):"You get into a heated argument through Facebook with your "+persons[personCount],
         ("sleep", "wake")  : "You take a nap at " + places[placeCount],
         ("dream", "wake")  : "You wake up from a nightmare",
         ("work", "eats")   : "You eat at your desk",
@@ -125,14 +116,14 @@ def setup(personCount, placeCount):
         ("flirt", "browse"): "You decide to go on Tinder",
         ("browse", "talk") : "You go on social media",
         ("talk", "browse") : "You go on social media",
-        ("flirt", "eat")   : "You go on a date with your " + persons[personCount],
-        ("eat", "flirt")   : "You go on a date with your " + persons[personCount],
+        ("flirt", "eat")   : "You go on a date with your "+persons[personCount],
+        ("eat", "flirt")   : "You go on a date with your "+persons[personCount],
         ("sleep", "cry")   : "You cry yourself to sleep",
         ("cry", "sleep")   : "You cry yourself to sleep",
         ("cry", "smile")   : "You cry tears of joy",
         ("smile", "cry")   : "You cry tears of joy",
-        ("smile", "fight") : "You playfight with " + persons[personCount],
-        ("fight", "smile") : "You playfight with " + persons[personCount],
+        ("smile", "fight") : "You playfight with "+persons[personCount],
+        ("fight", "smile") : "You playfight with "+persons[personCount],
         
         # Three Input
         ("sleep" , "wake", "work") : "You're late for work",
@@ -152,7 +143,7 @@ def setup(personCount, placeCount):
 
 def reset():
     response = []
-    count = 6
+    count = 1
     words = set([
         "flirt",      "eat", 
         "smile",     "talk", 
@@ -168,22 +159,21 @@ def reset():
 
 
 
-random_line = "You're fired from work"
+random_line = "You decide to go on Tinder"
 done = False
 response, count, words = reset()
 
 while not done:
-    current_time = ""
-    if (count % 12 == 0):
-        current_time += "12:00"
-    else:
-        current_time += str(count % 12)+":00"
-    if (count//12):
-        current_time += "PM: "
-    else:
-        current_time += "AM: "
+
+response = []
+count = 1
+print("\n Welcome to 'How to Human'\n"+"-"*25,"\n\n - Please select 10 words\n   to see how well you can\n   make your day.\n Press enter after typing each word.\n Certain sequences create specific events.")
+
+print("Words Left:",words)
+
+while True:
     # Take in user input
-    user = input(current_time);
+    user = input(str(count)+":");
     if user in words:
         if user == "end":
             parse(response)
@@ -191,7 +181,7 @@ while not done:
         words.remove(user)
         print("Words Left:",words)
         response.append(user)
-        count += 2;
+        count+=1;
 
     else:
         print("Not in list\n")
@@ -203,9 +193,9 @@ while not done:
             print(line)
             if random_line == line:
                 done = True
-        print("-"*10)
+                
         if not done:
-            print("\nThe line was not found\nTry again")
+            print("Try again")
             response, count, words = reset()
         else:
             print("-"*12,"\n- You win! -\n"+("-"*12))
